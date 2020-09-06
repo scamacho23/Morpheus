3d823e5a6f9cea4197161f273a723d423cb06344
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-09-06 11:33:56 -0400

    Add styling to install.sh file
    Added some styling to install.sh file.

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ INSTALL_LINK=https://github.com/scamacho23/morpheus
 
 # Create the Morpheus directory in /usr/local
 if [ ! -d "$MORPHEUS_DIRECTORY" ]; then
-    echo Cloning Morpheus directory in /usr/local...
+    echo \033[1mCloning Morpheus directory in /usr/local...\033[0m
     git clone "$INSTALL_LINK" "$MORPHEUS_DIRECTORY"
 fi
 
@@ -23,28 +23,28 @@ cd "$MORPHEUS_DIRECTORY" || exit
 
 # Create the installed directory in /usr/local
 if [ ! -d "$INSTALLED_DIRECTORY" ]; then
-    echo Creating Matrix directory in /usr/local...
-    echo \'Matrix\' is where your upgrades live \(upgrades are what we call your installed packages\)
+    echo \033[1mCreating Matrix directory in /usr/local...\033[0m
+    echo \033[1m\'Matrix\' is where your upgrades live \(upgrades are what we call your installed packages\)\033[0m
     mkdir "$INSTALLED_DIRECTORY"
 fi
 
 # Compile morpheus and add the binary to /usr/local/bin
 if [ -f "$BINARY_SYM_PATH" ]; then
     # exit if the binary is already installed
-    echo You seem to already have Morpheus installed on your system. Exiting...
+    echo \033[1mYou seem to already have Morpheus installed on your system. Exiting...\033[0m
     exit
 else
-    echo Building morph...
+    echo \033[1mBuilding morph...\033[0m
     # Create local bin to hold morph executable
     if [ ! -d "$BINARY_LOCAL_DIRECTORY" ]; then
-        echo Creating bin in /usr/local/Morpheus
+        echo \033[1mCreating bin in /usr/local/Morpheus...\033[0m
         mkdir "$BINARY_LOCAL_DIRECTORY"
     fi
     # compile morpheus
     make
     # move the compiled version to /usr/local/Morpheus
     mv "morph" "$BINARY_LOCAL_PATH" 
-    echo Symlinking morph into /usr/local/bin
+    echo \033[1mSymlinking morph into /usr/local/bin...\033[0m
     ln -s "$BINARY_LOCAL_PATH" "$BINARY_SYM_PATH"
 fi
 
