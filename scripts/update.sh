cc9bd4fc8174305f568090ed46b84b0f6bd9ecfe
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-09-06 17:27:22 -0400

    Update proper update.sh pull location

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,6 @@ MORPHEUS_REPOSITORY=/usr/local/Morpheus
 
 if [ -d "$MORPHEUS_REPOSITORY" ]; then
     echo Updating Morpheus
-    cd "$MORPHEUS_DIRECTORY" && git pull
+    cd "$MORPHEUS_REPOSITORY" && git pull
 fi
 
