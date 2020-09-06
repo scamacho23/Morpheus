1aec74027aa5a4739302c75bff38d0ce5c3325a3
scamacho23 <66191010+scamacho23@users.noreply.github.com>
2020-09-06 10:41:47 -0400

    Add to install.sh file
    Made it so that install.sh file clones the git repository of Morpheus
    online into a local Morpheus directory at /usr/local/Morpheus. This
    can all be performed by running sudo bash -e etc.

--- install.sh
+++ install.sh
@@ -1,39 +1,54 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 MORPHEUS_DIRECTORY=/usr/local/Morpheus 
 
-INSTALLED_DIRECTORY=/usr/local/Morpheus/installed
+INSTALLED_DIRECTORY=/usr/local/Matrix
 
-BINARY_PATH=/usr/local/bin/morph
+BINARY_SYM_PATH=/usr/local/bin/morph
 
-BINARY_LOCAL=/usr/local/Morpheus/morph
+BINARY_LOCAL_DIRECTORY=/usr/local/Morpheus/bin
+
+BINARY_LOCAL_PATH=/usr/local/Morpheus/morph
+
+INSTALL_LINK=https://raw.githubusercontent.com/scamacho23/morpheus/master/install.sh
+
 
 # Create the Morpheus directory in /usr/local
 if [ ! -d "$MORPHEUS_DIRECTORY" ]; then
-    echo Creating Morpheus directory in /usr/local...
-    mkdir "$MORPHEUS_DIRECTORY"
+    echo Cloning Morpheus directory in /usr/local...
+    git clone "$INSTALL_LINK" "$MORPHEUS_DIRECTORY"
 fi
 
-# Create the installed directory in /usr/local/Morpheus
+cd "$MORPHEUS_DIRECTORY" || exit
+
+# Create the installed directory in /usr/local
 if [ ! -d "$INSTALLED_DIRECTORY" ]; then
-    echo Creating installed directory in /usr/local/Morpheus...
+    echo Creating Matrix directory in /usr/local...
+    echo \'Matrix\' is where your upgrades live \(upgrades are what we call your installed packages\)
     mkdir "$INSTALLED_DIRECTORY"
 fi
 
 # Compile morpheus and add the binary to /usr/local/bin
-if [ -f "$BINARY_PATH" ]; then
+if [ -f "$BINARY_SYM_PATH" ]; then
     # exit if the binary is already installed
     echo You seem to already have Morpheus installed on your system. Exiting...
     exit
 else
     echo Building morph...
+    # Create local bin to hold morph executable
+    if [ ! -d "$BINARY_LOCAL_DIRECTORY" ]; then
+        echo Creating bin in /usr/local/Morpheus
+        mkdir "$BINARY_LOCAL_DIRECTORY"
+    fi
     # compile morpheus
     make
     # move the compiled version to /usr/local/Morpheus
-    cp "morph" "$BINARY_LOCAL" 
+    mv "morph" "$BINARY_LOCAL_PATH" 
     echo Symlinking morph into /usr/local/bin
-    ln -s "$BINARY_LOCAL" "$BINARY_PATH"
+    ln -s "$BINARY_LOCAL_PATH" "$BINARY_SYM_PATH"
 fi
+
+cd ~ || exit
 
 
 
